ad77dec68b19f55ae6d012dbd999b6bf14b73ebf
Tim Hawes <me@timhawes.com>
2011-02-09 12:19:37 +0000

    Rolled-up changes: Debug via standard logging framework. Sort deletions to handle a hierarchy correctly. Allow ignoring of specified attributes.

--- ldapwriter.py
+++ ldapwriter.py
@@ -6,25 +6,28 @@
 import ldap
 import ldap.modlist
 from ldap.functions import explode_dn
+import logging
 
 class LDAPWriter(object):
-    def __init__(self, conn, base, scope=ldap.SCOPE_ONELEVEL, dryrun=False, debug=False):
+    def __init__(self, conn, base, scope=ldap.SCOPE_ONELEVEL, dryrun=False, debug=False, ignore_attr_types=[]):
         self.conn   = conn
         self.base   = base
         self.scope  = scope
         self.dryrun = dryrun
         self.debug  = debug
+        self.ignore_attr_types = ignore_attr_types
         self.seen = {}
+        self.seen[str(explode_dn(base))] = True
 
     def put(self, dn, entry):
         status = None
         try:
             stored_object = self.conn.search_s(dn, ldap.SCOPE_BASE)[0]
-            mods = ldap.modlist.modifyModlist(stored_object[1], entry)
+            mods = ldap.modlist.modifyModlist(stored_object[1], entry, self.ignore_attr_types)
             if mods:
                 if self.debug:
-                    log("update: %s" % (dn))
-                    log(mods)
+                    logging.info("update: %s" % (dn))
+                    logging.info(mods)
                 if not self.dryrun:
                     self.conn.modify_s(dn, mods)
                 status = 'update'
@@ -35,7 +38,7 @@ class LDAPWriter(object):
         except ldap.NO_SUCH_OBJECT:
             mods = ldap.modlist.addModlist(entry)
             if self.debug:
-                log("add: %s" % (dn))
+                logging.info("add: %s" % (dn))
             if not self.dryrun:
                 self.conn.add_s(dn, mods)
             status = 'add'
@@ -46,9 +49,11 @@ class LDAPWriter(object):
         delete = []
         for dn, entry in self.conn.search_s(self.base, self.scope, attrlist=[]):
             if not self.seen.has_key(str(explode_dn(dn))):
-                if self.debug:
-                    log("delete: %s" % (dn))
-                if not self.dryrun:
-                    self.conn.delete_s(dn)
                 delete.append(dn)
+        delete.sort(lambda a, b: cmp(''.join(list(reversed(list(a)))), ''.join(list(reversed(list(b))))), reverse=True)
+        for dn in delete:
+            if self.debug:
+                logging.info("delete: %s" % (dn))
+            if not self.dryrun:
+                self.conn.delete_s(dn)
         return delete
