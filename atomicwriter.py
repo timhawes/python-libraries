21d60964783ce086126cc7902fe460303272865b
Tim Hawes <me@timhawes.com>
2016-05-23 00:13:57 +0100

    Option to compare old and new files in AtomicWriter

--- atomicwriter.py
+++ atomicwriter.py
@@ -11,6 +11,7 @@ import grp
 import os
 import pwd
 import tempfile
+import filecmp
 
 __all__ = ['AtomicWriter']
 
@@ -18,19 +19,19 @@ class AtomicWriter:
     """Safely writes to a file, avoiding incomplete or corrupted files by
     atomically renaming it after completion.
     """
-        
-    def __init__(self, filename, mode=None, tmp_suffix=None, user=None, group=None, detect_conflict=False):
+
+    def __init__(self, filename, mode=None, tmp_suffix=None, user=None, group=None, detect_conflict=False, compare=False):
         # tmp_suffix is deprecated and ignored
         self.filename = filename
         self.tempfd, self.tempfilename = tempfile.mkstemp(dir=os.path.dirname(filename))
         self.tempfile = os.fdopen(self.tempfd, "w")
         self.detect_conflict = detect_conflict
+        self.compare = compare
         if self.detect_conflict:
             try:
                 self.initial_stat = os.stat(self.filename)
             except OSError:
                 self.initial_stat = None
-                self.detect_conflict = None
         if mode:
             os.chmod(self.tempfilename, mode)
             self.auto_chmod = False
@@ -50,10 +51,10 @@ class AtomicWriter:
 
     def __len__(self):
         return int(self.tempfile.tell())
-    
+
     def write(self, data):
         self.tempfile.write(data)
-        
+
     def commit(self):
         self.tempfile.close()
         try:
@@ -67,26 +68,20 @@ class AtomicWriter:
                 os.chown(self.tempfilename, st.st_uid, st.st_gid)
         except OSError:
             pass
+        if self.compare:
+            if os.path.exists(self.filename) and filecmp.cmp(self.filename, self.tempfilename):
+                # new file is identical to the old one
+                os.unlink(self.tempfilename)
+                return
         os.rename(self.tempfilename, self.filename)
-        
+
     def rollback(self):
         self.tempfile.close()
         os.unlink(self.tempfilename)
-        
+
     def __del__(self):
         try:
             self.tempfile.close()
             os.unlink(self.tempfilename)
         except:
             pass
-
-if __name__ == "__main__":
-    f = AtomicWriter("hello.txt", mode=0644)
-    f.write("Hello World\n")
-    f.commit()
-    f = AtomicWriter("hello.txt", mode=0640)
-    f.write("So long...\n")
-    f.rollback()
-    f = AtomicWriter("hello.txt", mode=0600)
-    f.write("Farewell\n")
-    # leave file open and exit
